5fe967a8b3db3bf62a95beedd5ae1e4b152bb5ff
UserAntonio2023 <144562319+UserAntonio2023@users.noreply.github.com>
2023-10-28 11:59:19 -0700

    Update lexerSQL.js
    commit

--- lexerSQL.js
+++ lexerSQL.js
@@ -1,40 +1,50 @@
 const fs = require('fs');
-const readline = require('readline');
 
-const archivo = 'sql.txt';
-
-// Verificamos si el archivo existe
-if (!fs.existsSync(archivo)) {
-    console.log(`El archivo ${archivo} no existe.`);
-    return;
+// Funcion para cargar las palabras clave SQL desde un archivo de tokens
+function cargarPalabrasClaveSQL(archivoTokens) {
+  const palabrasClave = new Map();
+  const contenido = fs.readFileSync(archivoTokens, 'utf8');
+  const lineas = contenido.split('\n');
+  lineas.forEach((linea) => {
+    const [numero, palabra] = linea.split(': ');
+    if (palabra) {
+      palabrasClave.set(palabra.trim().toUpperCase(), numero);
+    }
+  });
+  return palabrasClave;
 }
 
-const rl = readline.createInterface({
-    input: fs.createReadStream(archivo, 'utf8')
-});
+// Funcion para tokenizar una consulta SQL
+function tokenizarConsulta(consulta, palabrasClave) {
+  const tokens = consulta.split(/\s+/);
+  const tokensTokenizados = [];
 
-let sentencia = ''; // Variable para almacenar la sentencia SQL
-
-rl.on('line', (line) => {
-    // Eliminar espacios en blanco al principio y al final de la línea
-    line = line.trim();
-
-    // Comprobar si la línea no está vacía
-    if (line.length > 0) {
-        sentencia += line + ' '; // Agregar la línea a la sentencia
+  tokens.forEach((token) => {
+    const tokenUpperCase = token.trim().toUpperCase();
+    if (palabrasClave.has(tokenUpperCase)) {
+      tokensTokenizados.push(`[${palabrasClave.get(tokenUpperCase)}: ${token}]`);
+    } else if (/^\d+$/.test(token)) {
+      tokensTokenizados.push(`[Número: ${token}]`);
+    } else {
+      tokensTokenizados.push(token);
     }
+  });
 
-    // Comprobar si la línea termina con punto y coma (fin de la sentencia)
-    if (line.endsWith(';')) {
-        console.log('\nSentencia:');
-        console.log(sentencia); // Mostrar la sentencia
-        sentencia = ''; // Reiniciar la variable para la próxima sentencia
-    }
-});
+  return tokensTokenizados.join(' ');
+}
 
-rl.on('close', () => {
-    if (sentencia.trim() !== '') {
-        console.log('\nSentencia:');
-        console.log(sentencia); // Mostrar la última sentencia si no termina con punto y coma
-    }
-});
+// Función principal
+function main(archivoTokens, archivoConsultas) {
+  const palabrasClaveSQL = cargarPalabrasClaveSQL(archivoTokens);
+  const consultas = fs.readFileSync(archivoConsultas, 'utf8').split('\n');
+
+  consultas.forEach((consulta, index) => {
+    const consultaTokenizada = tokenizarConsulta(consulta, palabrasClaveSQL);
+    console.log(`Sentencia ${index + 1}: ${consulta}`);
+    console.log(`Tokens: ${consultaTokenizada}\n`);
+  });
+}
+
+const archivoTokens = 'sql_keywords.txt'; // Nombre del archivo de tokens SQL
+const archivoConsultas = 'sql.txt'; // Nombre del archivo de consultas SQL
+main(archivoTokens, archivoConsultas);
