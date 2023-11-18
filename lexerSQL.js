376942456bead71f2b0de6d1c6cb0003c03fee82
UserAntonio2023 <al21760500@ite.edu.mx>
2023-11-18 11:35:54 -0800

    se añadio un validador de reglas sintacticas

--- lexerSQL.js
+++ lexerSQL.js
@@ -1,58 +1,99 @@
 const fs = require('fs');
 
+// Función para cargar las palabras clave desde un archivo y devolver un mapa
 function cargarPalabrasClaveSQL(archivoTokens) {
   const palabrasClave = new Map();
   const contenido = fs.readFileSync(archivoTokens, 'utf8');
   const lineas = contenido.split('\n');
+
+  // Iterar sobre cada línea del archivo
   lineas.forEach((linea) => {
     const [numero, palabra] = linea.split(': ');
+
+    // Añadir la palabra clave al mapa si existe
     if (palabra) {
       palabrasClave.set(palabra.trim().toUpperCase(), numero);
     }
   });
+
   return palabrasClave;
 }
 
+// Función para tokenizar una consulta usando las palabras clave proporcionadas
 function tokenizarConsulta(consulta, palabrasClave) {
   const tokens = consulta.split(/\s+/);
   const tokensTokenizados = [];
 
+  // Iterar sobre cada token en la consulta
   tokens.forEach((token) => {
     const tokenUpperCase = token.trim().toUpperCase();
+
+    // Verificar si el token es una palabra clave
     if (palabrasClave.has(tokenUpperCase)) {
       tokensTokenizados.push(`[${palabrasClave.get(tokenUpperCase)}: ${token}]`);
     } else if (/^\d+$/.test(token)) {
+      // Verificar si el token es un número
       tokensTokenizados.push(`[Número: ${token}]`);
     } else {
-      tokensTokenizados.push(`[Error: ${token}]`); 
+      // Si no es una palabra clave ni un número, se considera un error
+      tokensTokenizados.push(`[Error: ${token}]`);
     }
   });
 
   return tokensTokenizados;
 }
 
-function evaluarSentencia(sentencia) {
-  
-  // Retorna true si la evaluación es exitosa, false si hay algún error
- 
-  return true; 
+// Función para evaluar una sentencia usando un validador de reglas sintácticas
+function evaluarSentencia(consulta, reglasSintacticas) {
+  const tokens = consulta.split(/\s+/);
+
+  // Verificar si los tokens cumplen con las reglas sintácticas
+  for (const regla in reglasSintacticas) {
+    const reglaTokens = reglasSintacticas[regla];
+    if (tokens.length === reglaTokens.length) {
+      let esValida = true;
+      for (let i = 0; i < tokens.length; i++) {
+        if (parseInt(tokens[i]) !== reglaTokens[i]) {
+          esValida = false;
+          break;
+        }
+      }
+      if (esValida) {
+        console.log(`La regla sintáctica ${regla} es válida.`);
+        return true;
+      }
+    }
+  }
+
+  console.log("No se cumple ninguna regla sintáctica.");
+  return false;
 }
 
+// Función principal que carga las palabras clave, lee las consultas y las procesa
 function main(archivoTokens, archivoConsultas) {
   const palabrasClaveSQL = cargarPalabrasClaveSQL(archivoTokens);
+  const reglasSintacticas = {
+    1: [200, 10],  
+    
+  };
+
   const consultas = fs.readFileSync(archivoConsultas, 'utf8').split('\n');
 
+  // Iterar sobre cada consulta en el archivo
   consultas.forEach((consulta, index) => {
     const tokens = tokenizarConsulta(consulta, palabrasClaveSQL);
     console.log(`Sentencia ${index + 1}: ${consulta}`);
     console.log(`Tokens: ${tokens.join(' ')}\n`);
 
     // Evaluar la sentencia y mostrar si la evaluación fue exitosa o no
-    const evaluacionExitosa = evaluarSentencia(consulta);
+    const evaluacionExitosa = evaluarSentencia(consulta, reglasSintacticas);
     console.log(`Evaluación Exitosa: ${evaluacionExitosa}\n`);
   });
 }
 
+// Nombres de los archivos de entrada
 const archivoTokens = 'sql_keywords.txt'; // Nombre del archivo de tokens SQL
 const archivoConsultas = 'sql.txt'; // Nombre del archivo de consultas SQL
+
+// Llamada a la función principal
 main(archivoTokens, archivoConsultas);
